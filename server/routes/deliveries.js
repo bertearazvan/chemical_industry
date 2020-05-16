1e9d1a7431c1570d101f88ce415f3647bdbb3c23
Stefan Andrei <stefandrei123@gmail.com>
2020-05-16 15:48:30 +0200

    created deliveries.js

---
+++ server/routes/deliveries.js
@@ -0,0 +1,5 @@
+const router = require('express');
+
+const Deliveries = require('../models/Deliveries');
+const Warehouse = require('../models/Warehouse');
+const Company = require('../models/Company');
