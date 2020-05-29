3869e74e70801a84e0e50f6e897b48b40e25048a
bertearazvan <bertea74@gmail.com>
2020-05-29 18:29:14 +0200

    added the build folder

---
+++ client/build/precache-manifest.e93cae9a65dc344d6a1d6e332c94de27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cdf941ad3fafc8b46e665c30002e62",
+    "url": "/chemical_industry/client/build/index.html"
+  },
+  {
+    "revision": "6951310303fae0533e0e",
+    "url": "/chemical_industry/client/build/static/css/main.03180f2a.chunk.css"
+  },
+  {
+    "revision": "fd81e174dcc6bc841d56",
+    "url": "/chemical_industry/client/build/static/js/2.eb60a3ad.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/chemical_industry/client/build/static/js/2.eb60a3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6951310303fae0533e0e",
+    "url": "/chemical_industry/client/build/static/js/main.2cf225d7.chunk.js"
+  },
+  {
+    "revision": "509ec849dc280b186d71",
+    "url": "/chemical_industry/client/build/static/js/runtime-main.d4483934.js"
+  }
+]);
