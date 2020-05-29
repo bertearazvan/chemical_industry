94b0c048eb13ee69d91fe8f8866901e71a28135c
bertearazvan <bertea74@gmail.com>
2020-05-29 21:26:46 +0200

    Updates

--- precache-manifest.8d0ef2dae7d4714604e1441ef7934dfa.js
+++ precache-manifest.8d0ef2dae7d4714604e1441ef7934dfa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c274219e10f16e82df51ff3777447a2",
+    "revision": "8d84b20425507587ab89d07194e233a8",
     "url": "/chemical_industry/index.html"
   },
   {
-    "revision": "6f6c9dd1407625e209e8",
+    "revision": "f4be5adf4ffd1862d0e7",
     "url": "/chemical_industry/static/css/main.03180f2a.chunk.css"
   },
   {
-    "revision": "8af4e904e5a70f3290e1",
-    "url": "/chemical_industry/static/js/2.5be5fd2e.chunk.js"
+    "revision": "65fa44774a68f3e714b0",
+    "url": "/chemical_industry/static/js/2.353cfb8e.chunk.js"
   },
   {
     "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
-    "url": "/chemical_industry/static/js/2.5be5fd2e.chunk.js.LICENSE.txt"
+    "url": "/chemical_industry/static/js/2.353cfb8e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6f6c9dd1407625e209e8",
-    "url": "/chemical_industry/static/js/main.97996ce5.chunk.js"
+    "revision": "f4be5adf4ffd1862d0e7",
+    "url": "/chemical_industry/static/js/main.34e2023f.chunk.js"
   },
   {
     "revision": "f55263bb442adfd02a93",
