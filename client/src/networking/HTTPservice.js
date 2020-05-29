026c94254baf62c33b16caccb2a5056c03e7da49
bertearazvan <bertea74@gmail.com>
2020-05-29 20:58:13 +0200

    added the aws route

--- client/src/networking/HTTPservice.js
+++ client/src/networking/HTTPservice.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const setAuthToken = (token) => {
-  axios.defaults.baseURL = 'http://localhost:9090';
+  axios.defaults.baseURL = 'http://ec2-52-204-71-158.compute-1.amazonaws.com/';
   axios.defaults.headers.common['Authorization'] = token;
 };
